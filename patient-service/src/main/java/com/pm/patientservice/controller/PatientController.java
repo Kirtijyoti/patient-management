7516c85426c25cd9966edf1f731c26dab8a21981
Kirtijyoti Senapati <kirtijyotisenapati@gmail.com>
2026-01-22 22:36:53 +0530

    Added a HTTP request in intellij[TEST]
    Added a GET request to get data. Wil remove later if needed

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/patient")
+@RequestMapping("/patients")
 public class PatientController  {
    private final PatientService patientService;
 
