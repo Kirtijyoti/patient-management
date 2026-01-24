2349b6a8548b0a2698ae8e351a734b8a24e5797d
Kirtijyoti Senapati <kirtijyotisenapati@gmail.com>
2026-01-24 22:09:28 +0530

    created PatientRequestDTO.java

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,64 @@
+package com.pm.patientservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 100 , message = "Name cannot exceed 100 characters")
+    private String  name;
+
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotNull(message = "Date of birth is required")
+    private String dateOfBirth;
+
+    @NotNull(message = "Registered date is required")
+    private String registeredDate;
+
+    public @NotBlank(message = "Name is required") @Size(max = 100, message = "Name cannot exceed 100 characters") String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank(message = "Name is required") @Size(max = 100, message = "Name cannot exceed 100 characters") String name) {
+        this.name = name;
+    }
+
+    public @NotBlank(message = "Email is required") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "Email is required") String email) {
+        this.email = email;
+    }
+
+    public @NotNull(message = "Date of birth is required") String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(@NotNull(message = "Date of birth is required") String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public @NotNull(message = "Registered date is required") String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(@NotNull(message = "Registered date is required") String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public @NotBlank(message = "Address is required") String getAddress() {
+        return address;
+    }
+
+    public void setAddress(@NotBlank(message = "Address is required") String address) {
+        this.address = address;
+    }
+}
