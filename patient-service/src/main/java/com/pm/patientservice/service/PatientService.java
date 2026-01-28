b9b59558d0c9d277f24d1a13926f2b43f0333557
Kirtijyoti Senapati <kirtijyotisenapati@gmail.com>
2026-01-28 12:55:24 +0530

    added exception for duplicate email when creating new patients

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,6 +1,7 @@
 package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -24,6 +25,9 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("Email is already registered with another patient");
+        }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO)); //
         return PatientMapper.toDTO(newPatient);
     }
